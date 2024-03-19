fc8442d512a9acaf47c258d40bafeb5311079aa5
marksapiandante <marksapiandante@gmail.com>
2024-03-19 12:01:45 +0000

    update

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 function Alert({children, onClose}:Props) {
   return (
-    <div className="alert alert-primary" role="alert">
+    <div className="alert alert-primary alert-dismissible" role="alert">
   {children}
   <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={onClose}></button>
 </div>
--- src/components/DismissBtn.tsx
+++ src/components/DismissBtn.tsx
@@ -3,7 +3,6 @@ interface Props {
     onClick: () => void
 }
 
-
 function DismissBtn({onClick} : Props) {
   return (
     <button type="button" className="btn btn-alert" onClick={onClick}>Click me</button> 
